e49cc7f059887c77d3a68d06419eeb2338d31ff6
Tilottam Zope <tilottamzope1999@gmail.com>
2024-06-23 09:10:34 +0530

    Fully Completed Dashbard

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -10,28 +10,36 @@ import MenuItem from '@mui/material/MenuItem';
 import { IoTimerOutline } from "react-icons/io5";
 import { Button } from '@mui/material';
 import { Chart } from "react-google-charts";
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import { FaEye } from "react-icons/fa";
+import { FaPen } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import Pagination from '@mui/material/Pagination';
 
 const ITEM_HEIGHT = 48;
 
 export const data = [
-    ["Task", "Hours per Day"],
-    ["Work", 11],
-    ["Eat", 2],
-    ["Commute", 2],
-    ["Watch TV", 2],
-    ["Sleep", 7], // CSS-style declaration
+  ["Task", "Hours per Day"],
+  ["Work", 11],
+  ["Eat", 2],
+  ["Commute", 2],
+  ["Watch TV", 2],
+  ["Sleep", 7], // CSS-style declaration
 ];
 
 export const options = {
   'backgroundColor': 'transparent',
   'chartArea': { 'width': '100%', 'height': '100%' },
-  // 'color': 'white',
-  
+
 
 };
 
 const Dashboard = () => {
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [showBy, setShowBy] = React.useState('');
+  const [showCatBy, setShowCatBy] = React.useState('');
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -103,7 +111,434 @@ const Dashboard = () => {
             </div>
           </div>
         </div>
+
+        <div className="card shadow border-0 p-3 mt-4">
+          <h3 className='hd'>Best Selling Products</h3>
+
+          <div className="row cardFilters mt-3">
+            <div className="col-md-3">
+              <h4>SHOW BY</h4>
+              <FormControl size='small' className='w-100'>
+                <InputLabel id="demo-simple-select-label">Row</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={showBy}
+                  label="Age"
+                  onChange={(e) => setShowBy(e.target.value)}
+                  className='w-100'
+                // displayEmpty
+                >
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+            <div className="col-md-3">
+              <h4>CATEGORY BY</h4>
+              <FormControl size='small' className='w-100'>
+                <InputLabel id="demo-simple-select-label">Category</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={showCatBy}
+                  label="Age"
+                  onChange={(e) => setShowCatBy(e.target.value)}
+                  className='w-100'
+                // displayEmpty
+                >
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+          </div>
+
+          <div className="table-responsive mt-3">
+            <table className="table table-bordered v-align">
+              <thead className='thead-dark'>
+                <tr>
+                  <th>NO.</th>
+                  <th>PRODUCT</th>
+                  <th>CATEGORY</th>
+                  <th>BRAND</th>
+                  <th>PRICE</th>
+                  <th>STOCK</th>
+                  <th>RATING</th>
+                  <th>ORDER</th>
+                  <th>SALES</th>
+                  <th>ACTION</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>#1</td>
+                  <td>
+                    <div className="d-flex align-items-center productBox">
+                      <div className="row">
+                        <div className="col-2">
+                          <div className="imageWrapper">
+                            <div className="img">
+                              <img src="https://m.media-amazon.com/images/I/71Dtf-ydPYL.jpg" alt="Product Image Not Found" className='w-100' />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-10">
+                          <div className="info">
+                            <h6>Lorem ipsum dolor sit amet...</h6>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                          </div>
+                        </div>
+                      </div>
+
+
+                    </div>
+                  </td>
+                  <td>Category1</td>
+                  <td>newbrand</td>
+                  <td>
+                    <div style={{ width: '70px' }}>
+                      <del className='old'>Rs.100.00</del>
+                      <span className='new text-danger'>Rs.80.00</span>
+                    </div>
+                  </td>
+                  <td>30</td>
+                  <td>4.9(16)</td>
+                  <td>100</td>
+                  <td>Rs.10k</td>
+                  <td>
+                    <div className="actions d-flex align-items-center">
+                      <Button className='secondary' color='secondary'><FaEye /></Button>
+                      <Button className='success' color='success'><FaPen /></Button>
+                      <Button className='error' color='error'><MdDelete /></Button>
+                    </div>
+                  </td>
+
+                </tr>
+                <tr>
+                  <td>#1</td>
+                  <td>
+                    <div className="d-flex align-items-center productBox">
+                      <div className="row">
+                        <div className="col-2">
+                          <div className="imageWrapper">
+                            <div className="img">
+                              <img src="https://m.media-amazon.com/images/I/71Dtf-ydPYL.jpg" alt="Product Image Not Found" className='w-100' />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-10">
+                          <div className="info">
+                            <h6>Lorem ipsum dolor sit amet...</h6>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                          </div>
+                        </div>
+                      </div>
+
+
+                    </div>
+                  </td>
+                  <td>Category1</td>
+                  <td>newbrand</td>
+                  <td>
+                    <div style={{ width: '70px' }}>
+                      <del className='old'>Rs.100.00</del>
+                      <span className='new text-danger'>Rs.80.00</span>
+                    </div>
+                  </td>
+                  <td>30</td>
+                  <td>4.9(16)</td>
+                  <td>100</td>
+                  <td>Rs.10k</td>
+                  <td>
+                    <div className="actions d-flex align-items-center">
+                      <Button className='secondary' color='secondary'><FaEye /></Button>
+                      <Button className='success' color='success'><FaPen /></Button>
+                      <Button className='error' color='error'><MdDelete /></Button>
+                    </div>
+                  </td>
+
+                </tr>
+                <tr>
+                  <td>#1</td>
+                  <td>
+                    <div className="d-flex align-items-center productBox">
+                      <div className="row">
+                        <div className="col-2">
+                          <div className="imageWrapper">
+                            <div className="img">
+                              <img src="https://m.media-amazon.com/images/I/71Dtf-ydPYL.jpg" alt="Product Image Not Found" className='w-100' />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-10">
+                          <div className="info">
+                            <h6>Lorem ipsum dolor sit amet...</h6>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                          </div>
+                        </div>
+                      </div>
+
+
+                    </div>
+                  </td>
+                  <td>Category1</td>
+                  <td>newbrand</td>
+                  <td>
+                    <div style={{ width: '70px' }}>
+                      <del className='old'>Rs.100.00</del>
+                      <span className='new text-danger'>Rs.80.00</span>
+                    </div>
+                  </td>
+                  <td>30</td>
+                  <td>4.9(16)</td>
+                  <td>100</td>
+                  <td>Rs.10k</td>
+                  <td>
+                    <div className="actions d-flex align-items-center">
+                      <Button className='secondary' color='secondary'><FaEye /></Button>
+                      <Button className='success' color='success'><FaPen /></Button>
+                      <Button className='error' color='error'><MdDelete /></Button>
+                    </div>
+                  </td>
+
+                </tr>
+                <tr>
+                  <td>#1</td>
+                  <td>
+                    <div className="d-flex align-items-center productBox">
+                      <div className="row">
+                        <div className="col-2">
+                          <div className="imageWrapper">
+                            <div className="img">
+                              <img src="https://m.media-amazon.com/images/I/71Dtf-ydPYL.jpg" alt="Product Image Not Found" className='w-100' />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-10">
+                          <div className="info">
+                            <h6>Lorem ipsum dolor sit amet...</h6>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                          </div>
+                        </div>
+                      </div>
+
+
+                    </div>
+                  </td>
+                  <td>Category1</td>
+                  <td>newbrand</td>
+                  <td>
+                    <div style={{ width: '70px' }}>
+                      <del className='old'>Rs.100.00</del>
+                      <span className='new text-danger'>Rs.80.00</span>
+                    </div>
+                  </td>
+                  <td>30</td>
+                  <td>4.9(16)</td>
+                  <td>100</td>
+                  <td>Rs.10k</td>
+                  <td>
+                    <div className="actions d-flex align-items-center">
+                      <Button className='secondary' color='secondary'><FaEye /></Button>
+                      <Button className='success' color='success'><FaPen /></Button>
+                      <Button className='error' color='error'><MdDelete /></Button>
+                    </div>
+                  </td>
+
+                </tr>
+                <tr>
+                  <td>#1</td>
+                  <td>
+                    <div className="d-flex align-items-center productBox">
+                      <div className="row">
+                        <div className="col-2">
+                          <div className="imageWrapper">
+                            <div className="img">
+                              <img src="https://m.media-amazon.com/images/I/71Dtf-ydPYL.jpg" alt="Product Image Not Found" className='w-100' />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-10">
+                          <div className="info">
+                            <h6>Lorem ipsum dolor sit amet...</h6>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                          </div>
+                        </div>
+                      </div>
+
+
+                    </div>
+                  </td>
+                  <td>Category1</td>
+                  <td>newbrand</td>
+                  <td>
+                    <div style={{ width: '70px' }}>
+                      <del className='old'>Rs.100.00</del>
+                      <span className='new text-danger'>Rs.80.00</span>
+                    </div>
+                  </td>
+                  <td>30</td>
+                  <td>4.9(16)</td>
+                  <td>100</td>
+                  <td>Rs.10k</td>
+                  <td>
+                    <div className="actions d-flex align-items-center">
+                      <Button className='secondary' color='secondary'><FaEye /></Button>
+                      <Button className='success' color='success'><FaPen /></Button>
+                      <Button className='error' color='error'><MdDelete /></Button>
+                    </div>
+                  </td>
+
+                </tr>
+                <tr>
+                  <td>#1</td>
+                  <td>
+                    <div className="d-flex align-items-center productBox">
+                      <div className="row">
+                        <div className="col-2">
+                          <div className="imageWrapper">
+                            <div className="img">
+                              <img src="https://m.media-amazon.com/images/I/71Dtf-ydPYL.jpg" alt="Product Image Not Found" className='w-100' />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-10">
+                          <div className="info">
+                            <h6>Lorem ipsum dolor sit amet...</h6>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                          </div>
+                        </div>
+                      </div>
+
+
+                    </div>
+                  </td>
+                  <td>Category1</td>
+                  <td>newbrand</td>
+                  <td>
+                    <div style={{ width: '70px' }}>
+                      <del className='old'>Rs.100.00</del>
+                      <span className='new text-danger'>Rs.80.00</span>
+                    </div>
+                  </td>
+                  <td>30</td>
+                  <td>4.9(16)</td>
+                  <td>100</td>
+                  <td>Rs.10k</td>
+                  <td>
+                    <div className="actions d-flex align-items-center">
+                      <Button className='secondary' color='secondary'><FaEye /></Button>
+                      <Button className='success' color='success'><FaPen /></Button>
+                      <Button className='error' color='error'><MdDelete /></Button>
+                    </div>
+                  </td>
+
+                </tr>
+                <tr>
+                  <td>#1</td>
+                  <td>
+                    <div className="d-flex align-items-center productBox">
+                      <div className="row">
+                        <div className="col-2">
+                          <div className="imageWrapper">
+                            <div className="img">
+                              <img src="https://m.media-amazon.com/images/I/71Dtf-ydPYL.jpg" alt="Product Image Not Found" className='w-100' />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-10">
+                          <div className="info">
+                            <h6>Lorem ipsum dolor sit amet...</h6>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                          </div>
+                        </div>
+                      </div>
+
+
+                    </div>
+                  </td>
+                  <td>Category1</td>
+                  <td>newbrand</td>
+                  <td>
+                    <div style={{ width: '70px' }}>
+                      <del className='old'>Rs.100.00</del>
+                      <span className='new text-danger'>Rs.80.00</span>
+                    </div>
+                  </td>
+                  <td>30</td>
+                  <td>4.9(16)</td>
+                  <td>100</td>
+                  <td>Rs.10k</td>
+                  <td>
+                    <div className="actions d-flex align-items-center">
+                      <Button className='secondary' color='secondary'><FaEye /></Button>
+                      <Button className='success' color='success'><FaPen /></Button>
+                      <Button className='error' color='error'><MdDelete /></Button>
+                    </div>
+                  </td>
+
+                </tr>
+                <tr>
+                  <td>#1</td>
+                  <td>
+                    <div className="d-flex align-items-center productBox">
+                      <div className="row">
+                        <div className="col-2">
+                          <div className="imageWrapper">
+                            <div className="img">
+                              <img src="https://m.media-amazon.com/images/I/71Dtf-ydPYL.jpg" alt="Product Image Not Found" className='w-100' />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-10">
+                          <div className="info">
+                            <h6>Lorem ipsum dolor sit amet...</h6>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                          </div>
+                        </div>
+                      </div>
+
+
+                    </div>
+                  </td>
+                  <td>Category1</td>
+                  <td>newbrand</td>
+                  <td>
+                    <div style={{ width: '70px' }}>
+                      <del className='old'>Rs.100.00</del>
+                      <span className='new text-danger'>Rs.80.00</span>
+                    </div>
+                  </td>
+                  <td>30</td>
+                  <td>4.9(16)</td>
+                  <td>100</td>
+                  <td>Rs.10k</td>
+                  <td>
+                    <div className="actions d-flex align-items-center">
+                      <Button className='secondary' color='secondary'><FaEye /></Button>
+                      <Button className='success' color='success'><FaPen /></Button>
+                      <Button className='error' color='error'><MdDelete /></Button>
+                    </div>
+                  </td>
+
+                </tr>
+              </tbody>
+            </table>
+
+            <div className="d-flex tableFooter">
+              <p>Showing <b>12</b> of <b>60</b> results</p>
+              <Pagination count={10} color="primary" className='pagination'showFirstButton showLastButton/>
+            </div>
+          </div>
+
+
+        </div>
       </div>
+
+
     </div>
   )
 }
